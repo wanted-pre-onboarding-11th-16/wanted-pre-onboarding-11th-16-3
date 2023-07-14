6d5e6de38f7f872b2d1ea1141f58418174ec9b89
jioneee <qoxx3659@gmail.com>
2023-07-14 22:17:30 +0900

    style: detail page 스타일 추가

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { useEffect } from 'react';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import { useParams } from 'react-router-dom';
@@ -25,13 +26,18 @@ const DetailPage = () => {
         <Loader />
       ) : (
         <>
-          <h1>{title}</h1>
-          <div>{number}</div>
-          <div>{comments}</div>
-          <div>{parseDate(updated_at)}</div>
-          <div>{user.login}</div>
-          <img src={user.avatar_url} alt="user_avatar" />
-          <ReactMarkdown>{body}</ReactMarkdown>
+          <Container>
+            <h1>{title}</h1>
+            <TextBox>
+              <img className="image" src={user.avatar_url} alt="user_avatar" />
+              <h2 className="bold">#{number}</h2>
+
+              <Text>{parseDate(updated_at)}</Text>
+              <Text>{user.login}</Text>
+              <Text>코멘트: {comments}</Text>
+            </TextBox>
+            <ReactMarkdown>{body}</ReactMarkdown>
+          </Container>
         </>
       )}
     </Layout>
@@ -39,3 +45,41 @@ const DetailPage = () => {
 };
 
 export default DetailPage;
+
+const Container = styled.div`
+  box-sizing: border-box;
+  width: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  border-bottom: solid 1px black;
+  margin: 50px auto;
+  padding: 10px;
+  cursor: pointer;
+  .bold {
+    font-weight: 500;
+  }
+  .ad {
+    border: solid 2px blue;
+  }
+  img {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+  }
+  .comment {
+    text-align: right;
+  }
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 5px;
+  border-bottom: solid 1px black;
+`;
+
+const Text = styled.span`
+  box-sizing: border-box;
+  margin: auto 5px;
+`;
