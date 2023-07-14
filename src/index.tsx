096264e4a1cfa6cdef6b775e05fea7f11998b985
seongminn <berry9901@gmail.com>
2023-07-14 20:44:17 +0900

    fix: Context Provider 연결

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,19 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import IssueDetailProvider from './context/detailContext';
+import IssuesContextProvider from './context/IssuesContext';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <IssuesContextProvider>
+        <IssueDetailProvider>
+          <App />
+        </IssueDetailProvider>
+      </IssuesContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
